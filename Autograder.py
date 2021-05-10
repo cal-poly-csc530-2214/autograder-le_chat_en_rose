c41aa082e6319ab157eb188314dfffeb0bbc3370
nauticalmonkey <17054595+nauticalmonkey@users.noreply.github.com>
2021-05-09 19:24:52 -0700

    need to create error rules

--- Autograder.py
+++ Autograder.py
@@ -1,22 +1,35 @@
 
-def findTargets(target, numarray):
-    for i in range(0, len(numarray)-1):
-        for j in range(i,len(numarray)):
-            if (numarray[i]+numarray[j]) == target :
-                return (i,j)
+def findTargets(target_int, numarray_list_int):
+    for i in range(0, len(numarray_list_int)-1):
+        for j in range(i+1,len(numarray_list_int)):
+            if (numarray_list_int[i]+numarray_list_int[j]) == target_int :
+                tuple_end = (i,j)
+                return tuple_end
 
 
 def anylizeprogram(programfile):
     f = open("demofile.txt", "r")
+    feedback= []
+    i = 0
+    for line in f.readlines:
+        holder = eml(line)
+        if(holder):
+            feedback.append((eml(holder),i))
+        i+=1
+    return feedback
         
 
 
 def main():
     tartget = 42
-    numbers = [3,7,9,18,22,36,6]
+    numbers = [3,7,21,9,18,22,36,6]
     print(findTargets(tartget,numbers))
     
     
+def eml(lineToAnylize):
+    #define common rules
+    pass
+
 
 if __name__ == "__main__":
     main()
