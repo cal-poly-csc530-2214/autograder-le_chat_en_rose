5361503b28640d932f34344fc93527a8f671f60a
slin35 <slin35@calpoly.edu>
2021-05-09 21:54:02 -0700

    updated eml

--- Autograder.py
+++ Autograder.py
@@ -31,14 +31,32 @@ def main():
     tartget = 42
     numbers = [3,7,21,9,18,22,36,6]
     print(findTargets(tartget,numbers))
+
+    program = open("demofile.txt").readlines()
+    convert_To_Mpy(program)
+
     
 #eml rules applied to each line    
 def eml(lineToAnylize):
     #define common rules
     # range(a1; a2) then range(a1 + 1; a2)
     # a0 == a1 then False
-    for word in lineToAnylize:
-        pass
+    # range ({ 0 ,1},len
+    
+    if "range" in lineToAnylize:
+        after_range = lineToAnylize.find('(') + 1
+        before_comma = lineToAnylize.find(',')
+        to_be_update = lineToAnylize[after_range: before_comma]
+        updated = "{"+to_be_update + " + 1}"
+        lineToAnylize = lineToAnylize[: after_range] + updated + lineToAnylize[before_comma:]
+    elif "==" in lineToAnylize:
+        after_if = lineToAnylize.find('if') + 3
+        before_end = lineToAnylize.find(':')
+        updated = "{" + lineToAnylize[after_if: before_end] + ",False}"
+        lineToAnylize = lineToAnylize[: after_if] + updated + lineToAnylize[before_end:]
+
+    return lineToAnylize
+    
 
 
             
